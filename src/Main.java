3560c562ac32e29c4871f5413b95b62e4930d0a5
Bwire Phabiano <bwirephabiano939@gmail.com>
2019-10-03 18:21:30 +0300

    added a line to main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i <= 5; i++) { //commit to memory
             System.out.println(i + " Hello World!");
+            System.out.println(i + " Hello Everyone there!");
         }
     }
 }
