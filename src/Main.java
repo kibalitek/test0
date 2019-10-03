6ebea45e69c452d14d3ad67d8579daac3c990723
Bwire Phabiano <bwirephabiano939@gmail.com>
2019-10-03 18:12:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) { //commit to memory
+            System.out.println(i + " Hello World!");
+        }
+    }
+}
+
+
+// for loop
+// debugging a program
+//IntelliJ IDE
+//create a project
